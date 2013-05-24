6150b3e978e4d23357c86ba838cbdbcca4267a62
rlm33 <maeseraitokun@gmail.com>
2013-05-24 19:48:55 +0200

    D2 & D3

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :line_items
+  has_many :line_items, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+  attr_accessible :description, :image_url, :price, :title, :line_items
 
   default_scope order: 'title'
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -10,4 +14,13 @@ class Product < ActiveRecord::Base
 		:with => %r{\.(gif|jpg|png)$}i,
 		:message => 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			errors[:base] << 'Line Items present'
+			return false
+		end
+	end
 end
